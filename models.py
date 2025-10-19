6f3275fdce463f93c569bd073fe4dc44a00ac4a1
Connor Frank <conjfrnk@gmail.com>
2025-10-19 15:47:19 -0400

    Update password hashing method to use 'pbkdf2:sha256' for improved security

--- models.py
+++ models.py
@@ -71,7 +71,7 @@ class Admin(db.Model):
     created_tournaments = db.relationship('Tournament', backref='creator', lazy='dynamic')
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
